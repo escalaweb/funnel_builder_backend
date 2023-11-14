cb038388b1710535af00a158cf60ab63ee13c1d6
Alvaro Segovia <alvarosego01@gmail.com>
2023-11-14 15:38:53 -0400

    retry migraciones

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,8 @@ module.exports = {
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: "env/env.empty.prod", to: "env/.env.prod" }
+        { from: "env/env.empty.prod", to: "env/.env.prod" },
+        { from: "src/database/migrations", to: "migrations" }
       ],
     }),
   ],
